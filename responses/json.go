58f949b1652130d6ed7602b135970d23f33a1cbf
teten <teten.nugraha18@gmail.com>
2020-08-22 06:06:56 +0700

    make responses

---
+++ responses/json.go
@@ -0,0 +1,27 @@
+package responses
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func JSON(w http.ResponseWriter, statuscode int, data interface{}) {
+	w.WriteHeader(statuscode)
+	err := json.NewEncoder(w).Encode(data)
+	if err != nil {
+		fmt.Println(w, "%s", err.Error())
+	}
+}
+
+func ERROR(w http.ResponseWriter, statuscode int, err error) {
+	if err != nil {
+		JSON(w, statusCode, struct {
+			Error string `json:"error"`
+		}{
+			Error: err.Error(),
+		})
+		return
+	}
+	JSON(w, http.StatusBadRequest, nil)
+}
